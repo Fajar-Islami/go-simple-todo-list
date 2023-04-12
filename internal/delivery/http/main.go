8fda1b81bcd4c4bc38874ea22e0d7d79f1e9c3b2
Fajar-Islami <fajarislami.0702@gmail.com>
2023-04-12 17:23:22 +0700

    :hammer: remove logger

--- internal/delivery/http/main.go
+++ internal/delivery/http/main.go
@@ -13,13 +13,12 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
-	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/recover"
 )
 
 func HTTPRouteInit(containerConf *container.Container) {
 	app := fiber.New()
-	app.Use(logger.New())
+	// app.Use(logger.New())
 	app.Use(cors.New())
 	app.Use(recover.New())
 
